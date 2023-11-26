089fef7f4003fcae2ff6f8828b89a0f27ba8bb29
juliofl82 <151486186+juliofl82@users.noreply.github.com>
2023-11-26 00:53:59 -0300

    exercidio atualizado

---
+++ M2/atividade_funcoessemparametros/script.js
@@ -0,0 +1,59 @@
+
+// Desenvolva uma função que mostre no console a frase "Hello World!".
+
+// const nome1 = 'Hello'
+// const nome2 = 'World'
+
+// function helloWorld(){ 
+
+//     console.log(`${nome1} ${nome2} !`);
+// }
+
+// helloWorld()
+
+
+// Desenvolva uma função para saber se o(a) aluno(a) possui métricas para virar monitor(a).
+// Requisitos:
+// 100 de Entregas
+// 100 de Entrevistas Técnicas
+// 100 de Presença
+// Se os requisitos forem atingidos, mostre no console "Possível monitor(a)." Se os requisitos não forem atingidos, mostre no console "Selecionar outro(a) aluno(a)."
+
+// const entregas = 100
+// const entrevistasTecnicas = 100
+// const presenca = 100
+
+// function possivelMonitor(){
+//     if (entregas == 100 && entrevistasTecnicas == 100 && presenca == 100) {
+//         console.log("Possível monitor(a)");
+//     } else {
+//             console.log("Selecionar outro(a) aluno(a)");
+//         }
+// }
+    
+
+// possivelMonitor()
+
+
+
+// Desenvolva uma função que recebe três variáveis com valores numéricos e identifica qual deles é o maior.
+
+
+
+
+
+// Desenvolva uma função para mostrar no console a seguinte frase: "Meu nome é -nome e sobrenome-, e tenho -idade- anos".
+// Declare as seguintes variáveis dentro da função:
+// nome
+// sobrenome
+// idade
+
+const nome = "José"
+const sobrenome = "Fernando"
+const idade = 20
+
+function mostrarFrase(){
+    console.log(`Meu nome é ${nome} ${sobrenome} e tenho ${idade} anos`)
+}
+
+mostrarFrase()
