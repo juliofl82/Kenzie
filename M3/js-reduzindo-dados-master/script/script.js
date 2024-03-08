06d27a263b00698e132a6a403490ac1c7be19df0
juliofl82 <jfigueiredolamounier@gmail.com>
2024-03-07 22:23:00 -0300

    update
    update

--- M3/js-reduzindo-dados-master/script/script.js
+++ M3/js-reduzindo-dados-master/script/script.js
@@ -4,25 +4,30 @@
 const numbers = [20, 13, 50, 36, 97];
 
 function sumNumbers(array) {
-  return;
+  return array.reduce((accumulator, valorAtual) => accumulator + valorAtual, 0);
 }
-// console.log(sumNumbers(numbers));
+console.log(sumNumbers(numbers));
 
 //2) Agora utilizaremos o nosso array de produtos. Crie uma função que percorra todos os elementos desse array e retorne o valor total de todos os produtos do carrinho.
 function totalProducts(array) {
-  return;
+  return array.reduce((accumulator, product) => accumulator + product.price, 0);
 }
-// console.log(totalProducts(products));
+console.log(totalProducts(products));
 
 //3) Vamos deixar mais interessante? Utilizando o nosso array products, crie uma função que filtre apenas os produtos com tamanho GG e retorne a soma dos preços desses produtos.
 function totalProductsSize(array) {
-  return;
+  const produtosGG = array.filter(product => product.size ==="GG");
+  const total = produtosGG.reduce((accumulator, product) => accumulator + product.price, 0);
+  return total;
 }
-// console.log(totalProductsSize(products));
+console.log(totalProductsSize(products));
 
 //DESAFIO
 //4) Crie uma função que filtre os elementos em promoção e aplique 50% de desconto no preço deles. Após isso, some o valor total desses produtos.
 function totalProductsSale(array) {
-  return;
+  const onSale = array.filter(product => product.sale === true);
+  const total = onSale.reduce((accumulator, product) => accumulator + (product.price * 0.5), 0);
+  // const final = (total * 0.5);
+  return total;
 }
-// console.log(totalProductsSale(products));
+console.log(totalProductsSale(products));
