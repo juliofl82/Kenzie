b68c65071527b43ac5de6e9b714e26854ebefe55
juliofl82 <jfigueiredolamounier@gmail.com>
2024-04-18 21:22:55 -0300

    update

--- React/atividade-requisicao-useEffect/src/App.jsx
+++ React/atividade-requisicao-useEffect/src/App.jsx
@@ -1,9 +1,11 @@
-import { useState, useEffect } from 'react'
-
+import { useState, useEffect } from 'react';
+import { PostList } from './components/PostList';
+import { FavoriteList } from './components/FavoriteList';
 
 function App() {
-
   const [postList, setPostList] = useState([]);
+  const localFavoritePosts = localStorage.getItem("@MYFAVORITEPOSTS");
+  const [favoritePosts, setFavoritePosts] = useState(localFavoritePosts ? JSON.parse(localFavoritePosts) : []);
 
   useEffect(() => {
     const getPost = async () => {
@@ -14,19 +16,34 @@ function App() {
     getPost();
   }, [])
 
+  useEffect(() => {
+    localStorage.setItem("@MYFAVORITEPOSTS", JSON.stringify(favoritePosts))
+  }, [favoritePosts]);
+
+  const addFavoritePost = (post) => {
+    setFavoritePosts(prev => {
+
+      if (!prev.some(favoritePost => favoritePost.id === post.id)) {
+        return [...prev, post];
+      } else {
+        alert("Noticia já favoritada");
+        return prev;
+      }
+    });
+  };
+
+  const removeFavoritePost = (postid) => {
+    const newFavoritePosts = favoritePosts.filter(post => post.id !== postid);
+    setFavoritePosts(newFavoritePosts);
+  }
+
   return (
     <div className="App" >
-      <ul>
-        {postList.map(post => (
-          <li key={post.id}>
-            <span>{post.category} - {post.author}</span>
-            <h2>{post.title}</h2>
-            <p>{post.content}</p>
-          </li>
-        ))}
-      </ul>
+      <PostList postList={postList} addFavoritePost={addFavoritePost} />
+      <FavoriteList favoritePosts={favoritePosts} removeFavoritePost={removeFavoritePost} />
     </div>
   )
 }
-
 export default App
+
+
