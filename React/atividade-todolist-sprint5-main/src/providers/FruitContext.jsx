e59b9b6c8bbc3e884ee3fe97e9d39f1642c1827c
juliofl82 <jfigueiredolamounier@gmail.com>
2024-04-27 00:22:32 -0300

    update

---
+++ React/atividade-todolist-sprint5-main/src/providers/FruitContext.jsx
@@ -0,0 +1,11 @@
+import { createContext } from "react";
+
+export const FruitContext = createContext({});
+
+export const FruitProvider = ({children}) => {
+    return (
+        <FruitContext.Provider value={{}}>
+            {children}        
+        </FruitContext.Provider>
+    );
+}
