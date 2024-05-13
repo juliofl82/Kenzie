4f8d957ac4590d86b450dd7d5a83ae6d8af6b0a7
juliofl82 <jfigueiredolamounier@gmail.com>
2024-05-13 15:07:27 -0300

    update react

---
+++ React/TodoList_Zustand/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+
+function App() {
+  
+
+  return (
+    <h1>teste React</h1>
+  )
+}
+
+export default App
--- React/atividades-sprint6-main/src/services/api.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-export const api = axios.create({
-    baseURL: "https://todolist-api-mu.vercel.app/",
-    timeout: 8 * 1000,
-})
